7f6a7b24b194fad61501af55e0942ab47d415677
msdevelop <m.s.info@web.de>
2013-11-28 18:02:19 +0100

    Initial Structure

---
+++ BlackJack/src/Views/GameView.java
@@ -0,0 +1,8 @@
+package Views;
+
+import javax.swing.JFrame;
+
+public class GameView extends JFrame
+{
+	
+}
---
+++ BlackJack/src/Views/MenuView.java
@@ -0,0 +1,8 @@
+package Views;
+
+import javax.swing.JFrame;
+
+public class MenuView extends JFrame
+{
+	
+}
