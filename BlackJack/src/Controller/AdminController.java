34f883a54a8acc70cf9467cdb3b759e08fe5b236
G4mble <tomone33@web.de>
2014-02-21 15:10:59 +0100

    added functions to adminController

--- BlackJack/src/Controller/AdminController.java
+++ BlackJack/src/Controller/AdminController.java
@@ -1,12 +1,38 @@
 package Controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+import Main.MainPage;
+import Models.ListModel;
 import Views.AdminView;
 
-public class AdminController
+public class AdminController implements ActionListener
 {
 
+	LinkedList<ListModel> userList;
+	AdminView adminView;
+	
 	public AdminController()
 	{
-		new AdminView(this);
+		this.userList = MainPage.xmlController.getLoginList();
+		this.adminView = new AdminView(this, this.userList);
+	}
+
+	public void actionPerformed(ActionEvent aE)
+	{
+		if(aE.getActionCommand().equals("show"))
+		{
+			this.showProperties();
+		}
+	}
+	
+	public void showProperties()
+	{
+		int index = adminView.getSelectedItem();
+		
+		this.adminView.setPropUsername(this.userList.get(index).getUsername());
+		this.adminView.setPropChipcount(this.userList.get(index).getChipcount());
 	}
 }
