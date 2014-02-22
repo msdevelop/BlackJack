018b4229699a18721482982b2d4b10f903bcc7c1
G4mble <tomone33@web.de>
2014-02-22 16:32:36 +0100

    asd

--- BlackJack/src/Controller/AccountController.java
+++ BlackJack/src/Controller/AccountController.java
@@ -12,71 +12,83 @@ import Views.AccountView;
 
 public class AccountController implements ActionListener
 {
-	
+
 	// declaration--------------------------------
-	
-	private AccountView accountView;
-	private String actualUser;
-	
+
+	private AccountView	accountView;
+	private String		actualUser;
+
 	// constructor--------------------------------
-	
+
 	public AccountController(String user)
 	{
 		actualUser = user;
-		this.accountView = new AccountView(this, user);
+		accountView = new AccountView(this, user);
 	}
-	
+
 	// methods-----------------------------------
-	
+
 	// button actionCommands
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equals("changePassword")) {
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getActionCommand().equals("changePassword"))
+		{
 			String passwordDialog = JOptionPane.showInputDialog(null,
-			        "Bitte geben Sie ihr neues Passwort ein: ",
-			        "Passwort ändern", JOptionPane.OK_CANCEL_OPTION);
-			if (passwordDialog != null) {
-				if (passwordDialog.length() >= 4
-				        && passwordDialog.length() <= 10) {
-					try {
+					"Bitte geben Sie ihr neues Passwort ein: ",
+					"Passwort ändern", JOptionPane.OK_CANCEL_OPTION);
+			if (passwordDialog != null)
+				if ((passwordDialog.length() >= 4)
+						&& (passwordDialog.length() <= 10))
+				{
+					try
+					{
 						MainPage.xmlController.savePasswordToXML(actualUser,
-						        passwordDialog, 0);
-					} catch (TransformerException e1) {
+								passwordDialog, 0);
+					}
+					catch (TransformerException e1)
+					{
 						e1.printStackTrace();
 					}
 					JOptionPane.showMessageDialog(null,
-					        "Ihr Passwort wurde erfolgreich geändert.");
-				} else {
+							"Ihr Passwort wurde erfolgreich geändert.");
+				}
+				else
 					JOptionPane
-					        .showMessageDialog(null,
-					                "Das Passwort darf nicht länger als 10 und nicht kürzer als 4 Zeichen sein.");
-				}
-			}
-		} else
-			if (e.getActionCommand().equals("newChips")) {
+							.showMessageDialog(null,
+									"Das Passwort darf nicht länger als 10 und nicht kürzer als 4 Zeichen sein.");
+		}
+		else
+			if (e.getActionCommand().equals("newChips"))
+			{
 				Boolean result = MainPage.xmlController
-				        .getChipCount(actualUser);
-				if (result == true) {
-					try {
+						.getChipCount(actualUser);
+				if (result == true)
+					try
+					{
 						MainPage.xmlController.saveChipcountToXML(actualUser,
-						        200);
+								200);
 						JOptionPane
-						        .showMessageDialog(null,
-						                "Ihnen wurden erfolgreich neue Chips zugeteilt.");
-					} catch (TransformerFactoryConfigurationError e1) {
-						e1.printStackTrace();
-					} catch (TransformerException e1) {
+								.showMessageDialog(null,
+										"Ihnen wurden erfolgreich neue Chips zugeteilt.");
+					}
+					catch (TransformerFactoryConfigurationError e1)
+					{
 						e1.printStackTrace();
 					}
-				} else {
+					catch (TransformerException e1)
+					{
+						e1.printStackTrace();
+					}
+				else
 					JOptionPane.showMessageDialog(null,
-					        "Sie besitzen noch Chips.");
-				}
-				
-			} else
-				if (e.getActionCommand().equals("close")) {
-					this.accountView.dispose();
+							"Sie besitzen noch Chips.");
+
+			}
+			else
+				if (e.getActionCommand().equals("close"))
+				{
+					accountView.dispose();
 					new MenuController();
 				}
-		
 	}
 }
